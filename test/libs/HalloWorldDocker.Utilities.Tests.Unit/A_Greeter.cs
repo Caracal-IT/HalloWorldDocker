f0462097bffc2faa2a2a75baf0d96f1b6e8af4e7
EttieneMaré <ettienemare@gmail.com>
2023-01-26 14:21:16 +0200

    Check if build pass if tests pass

--- test/libs/HalloWorldDocker.Utilities.Tests.Unit/A_Greeter.cs
+++ test/libs/HalloWorldDocker.Utilities.Tests.Unit/A_Greeter.cs
@@ -20,6 +20,6 @@ public class A_Greeter {
     var message = _greeter.Greet(bob);
     
     // Assert
-    message.Should().Be( $"ZGood morning {bob}");
+    message.Should().Be( $"Good morning {bob}");
   }
 }
