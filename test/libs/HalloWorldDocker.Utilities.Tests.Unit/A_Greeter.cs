1de57e9a6bdfb3b037ec7a3199a50e019205af17
EttieneMaré <ettienemare@gmail.com>
2023-01-26 12:59:16 +0200

    Test broken test

--- test/libs/HalloWorldDocker.Utilities.Tests.Unit/A_Greeter.cs
+++ test/libs/HalloWorldDocker.Utilities.Tests.Unit/A_Greeter.cs
@@ -20,6 +20,6 @@ public class A_Greeter {
     var message = _greeter.Greet(bob);
     
     // Assert
-    message.Should().Be( $"Good morning {bob}");
+    message.Should().Be( $"Good morning 1{bob}");
   }
 }
